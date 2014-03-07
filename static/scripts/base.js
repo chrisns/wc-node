e5684a683bdf9a6ed7590ade1876418b2e95b562
Chris Nesbitt-Smith <chris.nesbitt-smith@wonga.com>
2014-03-07 13:58:37 +0000

    interim commit

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -5,60 +5,90 @@
 
 
     var googleClientId = '84086224013-u450n6r4dkgr51v3pom39cqgsefrnm83.apps.googleusercontent.com';
-    jrgGoogleAuth.init({'client_id':googleClientId, 'scopeHelp':['login', 'email', 'contacts']});
+    jrgGoogleAuth.init({'client_id':googleClientId, 'scopeHelp':['login', 'email',]});
 
     //do actual login
     var evtGoogleLogin ="evtGoogleLogin";
     $scope.googleLogin =function() {
-      jrgGoogleAuth.login({'extraInfo':{'user_id':true, 'emails':true}, 'callback':{'evtName':evtGoogleLogin, 'args':[]} });
+      jrgGoogleAuth.login({'extraInfo':{'emails':true}, 'callback':{'evtName':evtGoogleLogin, 'args':[]} });
     };
-
+/*
     $scope.googleInfo;
-    // @param {Object} googleInfo
-      // @param {Object} token Fields directly returned from google, with the most important being access_token (but there are others not documented here - see google's documentation for full list)
-        // @param {String} access_token
-      // @param {Object} [extraInfo]
-        // @param {String} [user_id]
-        // @param {Array} [emails] Object for each email
-          // @param {String} value The email address itself
-          // @param {String?} type ?
-          // @param {Boolean} primary True if this is the user's primary email address
-        // @param {String} [emailPrimary] User's primary email address (convenience field extracted from emails array, if exists)
+    @param {Object} googleInfo
+      @param {Object} token Fields directly returned from google, with the most important being access_token (but there are others not documented here - see google's documentation for full list)
+        @param {String} access_token
+      @param {Object} [extraInfo]
+        @param {String} [user_id]
+        @param {Array} [emails] Object for each email
+          @param {String} value The email address itself
+          @param {String?} type ?
+          @param {Boolean} primary True if this is the user's primary email address
+        @param {String} [emailPrimary] User's primary email address (convenience field extracted from emails array, if exists)
 
-      $scope.$on(evtGoogleLogin, function(evt, googleInfo) {
-        $scope.googleInfo = googleInfo;
-        console.log($scope.googleInfo.extraInfo.emailPrimary);
-      });
+*/
+    $scope.$on(evtGoogleLogin, function(evt, googleInfo) {
+      $scope.googleInfo = googleInfo;
+      console.log($scope.googleInfo.extraInfo.emailPrimary);
+    });
 
-      var checkbox, textbox;
-      textbox = $builder.addFormObject('capture', {
+
+    var formjson = [
+      {
         component: 'textInput',
         label: 'Name',
         placeholder: 'Your name',
+        description: '',
+        editable: false,
         // required: true,
-      });
-      checkbox = $builder.addFormObject('capture', {
+      },
+      {
+        component: 'textInput',
+        label: 'Face',
+        placeholder: 'Your face',
+        description: 'faces',
+        required: true,
+        name: 'f',
+      },
+      {
+        component: 'radio',
+        label: 'Nose',
+        description: 'How many noses do you have?',
+        options: ['1', '2']
+      },
+      {
         component: 'checkbox',
         label: 'Pets',
         description: 'Do you have any pets?',
         options: ['Dog', 'Cat']
-      });
-      // $scope.form = $builder.forms.capture;
-      return $scope.submit = function() {
-        return $validator.validate($scope, 'capture').success(function() {
-          // empty the form out
-          $builder.forms.capture.forEach(function(entry){
+      }
+    ];
 
-            console.log(entry);
-            $builder.removeFormObject('capture');
-          });
-          // submit it 
-          return console.log('succesa');
-        }).error(function() {
-          // display errors
-          return;
-        });
-      };
+    formjson.forEach(function(formElement) {
+      $builder.addFormObject('capture', formElement);
+    });
+
+    $scope.removeFormEntry = function(entry) {
+      console.log(entry.index);
+      $builder.removeFormObject('capture', entry.index);
+    }
+
+
+    $scope.submit = function() {
+      return $validator.validate($scope, 'capture').success(function() {
+
+        // @TODO:submit it 
+
+        // on success of submit empty the form and get the next one
+        for (var i = ($builder.forms.capture.length - 1); i >= 0; i--) {
+          console.log($builder.forms.capture[i]);
+          $builder.removeFormObject('capture', $builder.forms.capture[i].index);
+        }
+        return true;
+      }).error(function() {
+        // display validation errors
+        return false;
+      });
+    };
 
 
   });
