115e97b9b874a30c057c50b226d026e8ae2fea70
Chris Nesbitt-Smith <chris.nesbitt-smith@wonga.com>
2014-03-10 15:02:14 +0000

    remove spiffworkflow from codebase, will bring back as a submodule

--- SpiffWorkflow2/storage/__init__.py
+++ SpiffWorkflow2/storage/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
-from SpiffWorkflow.version import __version__
-from SpiffWorkflow.Workflow import Workflow
-from SpiffWorkflow.Task import Task
-from SpiffWorkflow.exceptions import WorkflowException
+from .OpenWfeXmlSerializer import OpenWfeXmlSerializer
+from .XmlSerializer import XmlSerializer
+from .DictionarySerializer import DictionarySerializer
+from .JSONSerializer import JSONSerializer
 
 import inspect
 __all__ = [name for name, obj in list(locals().items())
