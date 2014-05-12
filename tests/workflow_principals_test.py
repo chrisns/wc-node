f24b986c0af59a4b1a926442b53eaadaad4f715a
Chris Nesbitt-Smith <chris.nesbitt-smith@wonga.com>
2014-05-12 15:41:24 +0100

    split test workflow spec into a seperate file for reuse

--- tests/workflow_principals_test.py
+++ tests/workflow_principals_test.py
@@ -23,36 +23,7 @@ from SpiffWorkflow.storage import DictionarySerializer
 from SpiffWorkflow.Task import *
 import unittest
 import json
-
-
-class TestWorkflowSpec(WorkflowSpec):
-    """Test workflow to work with"""
-    def __init__(self):
-        WorkflowSpec.__init__(self)
-        task_a1 = UserInput(self, 'task_a1', args=["name", "face", "nose", "pets"])
-        self.start.connect(task_a1)
-        task_a2 = UserInput(self, 'task_a2', args=["sid", "mildrid"])
-        task_a1.connect(task_a2)
-
-        excl_choice_1 = ExclusiveChoice(self, 'excl_choice_1')
-        task_a2.connect(excl_choice_1)
-
-        # default choice
-        task_b1 = Simple(self, 'task_b1')
-        excl_choice_1.connect(task_b1)
-
-
-        # choice
-        task_b2 = Simple(self, 'task_b2')
-        cond = Equal(Attrib('mildrid'), "va")
-        excl_choice_1.connect_if(cond, task_b2)
-
-        # end
-        end = Simple(self, 'End')
-        task_b1.connect(end)
-        task_b2.connect(end)
-
-
+from TestWorkflowSpec import TestWorkflowSpec
 
 
 class WorkflowFunctionalTests(unittest.TestCase):
