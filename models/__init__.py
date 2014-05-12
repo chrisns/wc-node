bbc03d5977a3b99e432c6e5dc9f005ad92a93aca
Chris Nesbitt-Smith <chris.nesbitt-smith@wonga.com>
2014-05-12 15:39:46 +0100

    split out requests and responses models into a seperate file

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 from .Execution import Execution
+from .api_requests_and_responses import *
 
 import inspect
 __all__ = [name for name, obj in list(locals().items())
