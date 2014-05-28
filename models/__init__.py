b5f6dff93878e7ba90259efeec6a65ac8ae4f9ee
Chris Nesbitt-Smith <chris@cns.me.uk>
2014-05-28 08:19:06 +0100

    emptying files that didn't need anything in

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division
-from .Execution import Execution
-from .api_requests_and_responses import *
-
-import inspect
-__all__ = [name for name, obj in list(locals().items())
-           if not (name.startswith('_') or inspect.ismodule(obj))]
--- tests/model_tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division
-from .Execution_test import *
-
-import inspect
-__all__ = [name for name, obj in list(locals().items())
-           if not (name.startswith('_') or inspect.ismodule(obj))]
