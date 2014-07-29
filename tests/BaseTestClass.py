c6cf6a32b653abaa1f8ac49b768b548e8dcf61fd
Chris Nesbitt-Smith <chris@cns.me.uk>
2014-07-29 08:31:05 +0100

    update coveragerc to run on synology box

--- tests/BaseTestClass.py
+++ tests/BaseTestClass.py
@@ -1,11 +1,9 @@
-__author__ = 'new-user'
 from google.appengine.ext import testbed
 import unittest
 
 class BaseTestClass(unittest.TestCase):
     def setupTestbed(self):
         self.testbed = testbed.Testbed()
-        # needed because endpoints expects a . in this value
         self.testbed.setup_env(current_version_id='testbed.version')
         self.testbed.activate()
         self.testbed.init_datastore_v3_stub()
