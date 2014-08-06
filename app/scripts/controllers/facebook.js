1e17d23e4bb3ac336aa0d381d30b89acc2faf7f1
Chris Nesbitt-Smith <chris@cns.me.uk>
2014-08-06 15:32:52 +0100

    facebook logic moved into a minimalist controller with tests

--- app/scripts/controllers/facebook.js
+++ app/scripts/controllers/facebook.js
@@ -14,77 +14,34 @@ define(['angular'], function (angular) {
     .config(['FacebookProvider', function (FacebookProvider) {
       FacebookProvider.init('665447500158300');
     }])
-    .controller('FacebookCtrl', function ($scope, Facebook) {
+    .controller('FacebookCtrl', function ($rootScope, $scope, Facebook) {
       // Define user empty data :/
-      $scope.user = {};
+      $rootScope.user = {};
+      $scope.facebookReady = Facebook.isReady;
 
       /**
        * Watch for Facebook to be ready.
        * There's also the event that could be used
        */
-      $scope.$watch(
-        function () {
-          return Facebook.isReady();
-        },
-        function (newVal) {
-//          if (newVal)
-          $scope.facebookReady = true;
-          Facebook.getLoginStatus(function (response) {
-            if (response.status === 'connected') {
-              $scope.me();
-              $scope.updateAccessToken();
-            }
+      $scope.$on('Facebook:authResponseChange', function(event, response) {
+        if (response.status === 'connected') {
+          Facebook.api('/me', function (response) {
+            $rootScope.user = response;
           });
         }
-      );
+        else {
+          $rootScope.user = {};
+        }
+      });
 
       /**
        * Login
        */
-      $scope.login = function () {
-        Facebook.login(function (response) {
-          if (response.status === 'connected') {
-            $scope.me();
-            $scope.updateAccessToken();
-          }
-        });
-      };
-
-
-      $scope.updateAccessToken = function () {
-        Facebook.getLoginStatus(function (response) {
-          $scope.$apply(function () {
-            $scope.authResponse = response.authResponse;
-          });
-        });
-      };
-
-      /**
-       * me
-       */
-      $scope.me = function () {
-        Facebook.api('/me', function (response) {
-          $scope.$apply(function () {
-            $scope.user = response;
-          });
-        });
-      };
+      $scope.login = Facebook.login;
 
       /**
        * Logout
        */
-      $scope.logout = function () {
-        Facebook.logout(function () {
-          $scope.$apply(function () {
-            $scope.user = {};
-            $scope.logged = false;
-          });
-        });
-      };
-      $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-      ];
+      $scope.logout = Facebook.logout;
     });
 });
--- test/spec/controllers/facebookSpec.js
+++ test/spec/controllers/facebookSpec.js
@@ -1,25 +1,90 @@
 /*jshint unused: vars */
-define(['angular', 'angular-mocks', 'app', 'facebook'], function(angular, mocks, app, Facebook) {
+define(['angular', 'facebook'], function (angular) {
   'use strict';
-
   describe('Controller: FacebookCtrl', function () {
 
     // load the controller's module
     beforeEach(module('wcApp.controllers.FacebookCtrl'));
+    beforeEach(module('facebook'));
 
     var FacebookCtrl,
+      mockFb,
+      fbready,
+      fbloginstatus,
       scope;
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function ($controller, $rootScope) {
+    beforeEach(inject(function ($controller, Facebook, $rootScope) {
       scope = $rootScope.$new();
+      mockFb = Facebook;
       FacebookCtrl = $controller('FacebookCtrl', {
-        $scope: scope
+        $scope: scope,
+        Facebook: mockFb
       });
     }));
-    console.error(Facebook);
-    it('should attach a list of awesomeThings to the scope', function () {
-      expect(scope.awesomeThings.length).toBe(3);
+
+    it('should initially say facebook is not ready', function () {
+      // Arrange
+      spyOn(mockFb, 'isReady').andReturn(false);
+      // Act
+      scope.$apply();
+      // Assert
+      expect(scope.facebookReady()).toBe(false);
     });
+
+
+    it('should start with an empty user', function () {
+      // Assert
+     expect(scope.user).toEqual({});
+    });
+
+    it('should delegate login to Facebook', function () {
+      // Assert
+      expect(scope.login).toBe(mockFb.login);
+    });
+
+    it('should delegate logout to Facebook', function () {
+      // Assert
+      expect(scope.logout).toBe(mockFb.logout);
+    });
+
+    it('should delegate facebookReady to Facebook', function () {
+      // Assert
+      expect(scope.facebookReady).toBe(mockFb.isReady);
+    });
+
+
+    it('should update $user if response changes to connected', function(){
+      // Arrange
+      var expectedUser = {
+        id: "12354",
+        first_name: "John",
+        last_name: "Smith",
+        name: "John Smith",
+      };
+      spyOn(mockFb, 'api').andCallFake(function(param, callback) {
+        return callback(expectedUser);
+      });
+
+      // Act
+      scope.$broadcast('Facebook:authResponseChange', {status: 'connected'});
+      scope.$apply();
+      expect(scope.user).toEqual(expectedUser);
+      expect(mockFb.api).toHaveBeenCalled();
+    });
+
+    it('should update $user if response changes to NOT connected', function () {
+      // Arrange
+      spyOn(mockFb, 'api');
+
+      // Act
+      scope.$broadcast('Facebook:authResponseChange', {status: 'notconnected'});
+      scope.$apply();
+
+      // Assert
+      expect(scope.user).toEqual({});
+      expect(mockFb.api).not.toHaveBeenCalled();
+    });
+
   });
 });
