9836a3440642b79a0ebc99535c27e4e233bd0274
Chris Nesbitt-Smith <chris.nesbitt-smith@wonga.com>
2014-12-02 14:11:03 +0000

    initial commit of node app

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var orientdb = require("orientdb");
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+var dbConfig = {
+    user_name: "admin",
+    user_password: "admin"
+};
+var serverConfig = {
+    host: "localhost",
+    port: 2424
+};
+
+var server = new orientdb.Server(serverConfig);
+var db = new orientdb.GraphDb("blog", server, dbConfig);
+
+db.open(function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log("Successfully connected to OrientDB");
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+routes.init(db, function(err) {
+    if (err) {
+        throw err;
+    }
+});
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+module.db = null;
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+exports.init = function(orient, callback) {
+  module.db = orient;
+};
